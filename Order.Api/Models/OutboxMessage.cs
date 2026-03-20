17c3bfae55b6555597e0c1d986566eae00cdf644
emilnordstroem <emilstoeve@outlook.com>
2026-03-20 09:49:38 +0100

    OutboxMessage created

---
+++ Order.Api/Models/OutboxMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Order.Api.Models
+{
+	public class OutboxMessage
+	{
+		public Guid Id { get; set; } = Guid.NewGuid();
+		public required string Type { get; set; }
+		public required string Payload { get; set; }
+		public DateTime? ProcessedAtUTC { get; set; }
+
+		public OutboxMessage ()
+		{
+
+		}
+	}
+}
